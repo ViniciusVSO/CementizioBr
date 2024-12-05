0cb35f022781e5128e7f97a5c90fe33189afec8b
ViniciusVSO <vinicius.so992@gmail.com>
2024-12-05 01:11:07 -0300

    fix vercel app

--- src/app/components/sub-components/form.tsx
+++ src/app/components/sub-components/form.tsx
@@ -23,20 +23,8 @@ const formDataSchema = z.object({
 
 type FormDataSchema = z.infer<typeof formDataSchema>
 
-const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
-  
-  const day = String(date.getDate()).padStart(2, '0'); 
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-  const hours = String(date.getHours()).padStart(2, '0'); 
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-
-  return `${hours}:${minutes} ${day}/${month}/${year}`;
-};
-
 export default function Form() {
-  const { register, handleSubmit, setError, formState } = useForm<FormDataSchema>({
+  const { register, handleSubmit, formState } = useForm<FormDataSchema>({
     resolver: zodResolver(formDataSchema),
   });
   const [isLoading, setIsLoading] = useState(false);
